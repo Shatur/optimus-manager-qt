16261fb3889e52604e0a33f6746d421198a09e63
Shatur95 <genaloner@gmail.com>
2019-02-27 11:46:49 +0200

    Bump version to 1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     SingleApplication app(argc, argv);
     SingleApplication::setApplicationName("Optimus Manager");
     SingleApplication::setOrganizationName("optimus-manager");
-    SingleApplication::setApplicationVersion("0.9.0");
+    SingleApplication::setApplicationVersion("1.0.0");
 
     // Set localization
     AppSettings settings;
