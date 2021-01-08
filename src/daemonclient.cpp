e57a142846806785029b2c234745d001f3143f60
Shatur95 <genaloner@gmail.com>
2021-01-08 22:23:26 +0200

    Update copyrights

--- src/daemonclient.cpp
+++ src/daemonclient.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright © 2019-2020 Hennadii Chernyshchyk <genaloner@gmail.com>
+ *  Copyright © 2019-2021 Hennadii Chernyshchyk <genaloner@gmail.com>
  *
  *  This file is part of Optimus Manager Qt.
  *
