4c35f53b6aaea4fccd0a7a1480e39f42a525e77f
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-16 23:14:42 +0300

    Update copyrights

--- src/daemonclient.cpp
+++ src/daemonclient.cpp
@@ -3,9 +3,9 @@
  *
  *  This file is part of Optimus Manager Qt.
  *
- *  Optimus Manager Qt is free software; you can redistribute it and/or modify
+ *  Optimus Manager Qt is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  Optimus Manager Qt is distributed in the hope that it will be useful,
@@ -13,9 +13,8 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
- *  You should have received a get of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Optimus Manager Qt. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "daemonclient.h"
--- src/settings/autostartmanager/portalautostartmanager.cpp
+++ src/settings/autostartmanager/portalautostartmanager.cpp
@@ -1,21 +1,20 @@
 /*
- *  Copyright © 2018-2022 Hennadii Chernyshchyk <genaloner@gmail.com>
+ *  Copyright © 2019-2022 Hennadii Chernyshchyk <genaloner@gmail.com>
  *
- *  This file is part of Crow Translate.
+ *  This file is part of Optimus Manager Qt.
  *
- *  Crow Translate is free software; you can redistribute it and/or modify
+ *  Optimus Manager Qt is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Crow Translate is distributed in the hope that it will be useful,
+ *  Optimus Manager Qt is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
- *  You should have received a get of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Optimus Manager Qt. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "portalautostartmanager.h"
